3f3cb712b7d75afa544c858d66836b7620eb396a
88lightningcreations <88lightningcreations@gmail.com>
2025-04-14 11:57:03 -0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -41,7 +41,7 @@ export const faqData = [
   {
     question: "How long does an HVAC system typically last?",
     answer:
-      "With proper maintenance, an HVAC system typically lasts 15-20 years. However, this can vary based on usage patterns, local climate, and how well the system is maintained.",
+      "With proper maintenance, an HVAC system typically lasts 10-15 years. However, this can vary based on usage patterns, local climate, and how well the system is maintained.",
   },
   {
     question: "What size HVAC system do I need for my home?",
@@ -51,12 +51,12 @@ export const faqData = [
   {
     question: "Common repair costs?",
     answer: 
-      "Our clients usually pay no more than $100-$300 for their common fixes!",
+      "Our clients usually pay between than $100-$300 for their common fixes!",
   },
   {
     question: "How can I improve my indoor air quality?",
     answer:
-      "You can improve indoor air quality by regularly changing air filters, installing air purifiers or UV lights, ensuring proper ventilation, using a humidifier or dehumidifier as needed, and scheduling regular duct cleaning.",
+      "You can improve indoor air quality by regularly changing air filters, installing air purifiers or UV lights, ensuring proper ventilation, using a humidifier or dehumidifier as needed, and scheduling regular duct sealing.",
   },
   {
     question: "What's the difference between a heat pump and a furnace?",
